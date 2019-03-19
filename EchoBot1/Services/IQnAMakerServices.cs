c3a67d78d7151307e411f6ec515e3b9cadec822a
nicolas sargos <lamenagerefolle@hotmail.com>
2019-03-19 14:12:52 +0100

    Ajout de la solution

---
+++ EchoBot1/Services/IQnAMakerServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Bot.Builder.AI.QnA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Services
+{
+    public interface IQnAMakerServices
+    {
+
+        QnAMaker QnAMaker { get; }
+    }
+}
