57cebdafbb4aeb71504a6ddef7f15c5dfe3f9243
nicolas sargos <lamenagerefolle@hotmail.com>
2019-03-21 12:56:11 +0100

    ajout de l'enregistrement des envois

--- ChatBotMiddleWare/ChatBotMiddleWare/Services/ConversationManagerService.cs
+++ ChatBotMiddleWare/ChatBotMiddleWare/Services/ConversationManagerService.cs
@@ -1,5 +1,6 @@
 ﻿using ChatBotMiddleWare.Helpers;
 using ChatBotMiddleWare.Models;
+using DAL.Context;
 using Microsoft.Bot.Connector.DirectLine;
 using System.Linq;
 
@@ -8,11 +9,19 @@ namespace ChatBotMiddleWare.Services
     public class ConversationManagerService
     {
         private readonly string directLineSecret = "DxFVyxUZjIk.jHP0wNdNMkRohtMA3U5_jS6UBs2eRtFDK_C1HsNXlis";
+        private ChatBotContext _context;
+
+        public ConversationManagerService()
+        {
+            _context = new ChatBotContext();
+        }
+
 
         private DirectLineClient CreateNewDirectLineClient()
         {
             var directLineClient = new DirectLineClient(directLineSecret);
             var newConversation = directLineClient.Conversations.StartConversation();
+            
             ConversationMemoryCache.Add(newConversation.ConversationId, directLineClient);
             return directLineClient;
         }
@@ -21,6 +30,16 @@ namespace ChatBotMiddleWare.Services
         {
             var directLineClient = new DirectLineClient(directLineSecret);
             var newConversation = directLineClient.Conversations.StartConversation();
+
+
+            // Ajoute la conversation à la base
+            DAL.Entities.Conversation conversation = new DAL.Entities.Conversation()
+            {
+                id = newConversation.ConversationId
+            };
+            _context.Conversations.Add(conversation);
+
+
             ConversationMemoryCache.Add(newConversation.ConversationId, directLineClient);
             return newConversation.ConversationId;
         }
@@ -45,6 +64,18 @@ namespace ChatBotMiddleWare.Services
                 Text = question.text,
                 Type = ActivityTypes.Message
             };
+
+
+            // Ajoute la activity à la base
+            DAL.Entities.Activity activity = new DAL.Entities.Activity()
+            {
+                conversationId = question.conversationId,
+                 text = question.text,
+                 type = ActivityTypes.Message
+            };
+            _context.Activities.Add(activity);
+
+
 
             client.Conversations.PostActivity(question.conversationId, userMessage);
             var msg = client.Conversations.GetActivities(question.conversationId).Activities.Last();
