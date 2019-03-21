56887f5049e186bf37c7b8a39798b09f2545c422
lamenagere <nicolassargos@hotmail.com>
2019-03-21 07:34:43 +0100

    Ajout du client

---
+++ ChatBotClient/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Chatbot/>
+  </div>
+</template>
+
+<script>
+import Chatbot from './components/Chatbot.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chatbot
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'assets/css/style.scss';
+</style>
---
+++ ChatBotClient/src/components/Bubble.vue
@@ -0,0 +1,41 @@
+<template>
+    <!-- <div :class="type">{{text}}</div> -->
+    <div>
+     <!-- bot answer -->
+        <div v-if= "type=='answer'" class="bot-message-container">
+          <div class="img-avatar-container"></div>
+          <div class="chat-bubble bot">
+            <vue-markdown  :source="text">
+            </vue-markdown>
+          </div>
+        </div>
+     <!-- bot images answer -->
+        <!-- <div v-if= "type=='answer'  && images != ''" class="bot-message-container">
+          <div class="img-avatar-container"></div>
+          <div class="chat-bubble bot">
+            img
+          </div>
+        </div> -->
+        <!-- user question -->
+        <div v-else class="user-message-container">
+          <div class="chat-bubble user">
+            <vue-markdown :source="text">
+            </vue-markdown>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown';
+
+export default {
+  components: {
+    VueMarkdown
+  },
+  props : ["type","text","images"]
+}
+</script>
+<style>
+
+</style>
